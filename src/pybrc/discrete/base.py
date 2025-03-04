b4a797d971f4862f82724064af12dccd67c4b817
Seung Hyun Kim <skim449@illinois.edu>
2025-03-04 17:03:37 -0600

    add default value for binsize

--- src/pybrc/discrete/base.py
+++ src/pybrc/discrete/base.py
@@ -12,7 +12,7 @@ from miv.core.operator.wrapper import cache_call
 
 @dataclass
 class BaseParseInput(OperatorMixin):
-    binsize: float
+    binsize: float = 0.001  # sec
 
     TTL_state: int = 1
     tag: str = "input parsing"
@@ -35,18 +35,18 @@ class BaseParseInput(OperatorMixin):
         ax2 = ax1.twinx()
 
         ax1.plot(timestamps, states)
-        ax2.plot(time, y, 'or')
+        ax2.plot(time, y, "or")
 
-        ax1.set_xlabel('time (sec)')
-        ax1.set_ylabel('TTL state')
-        ax2.set_ylabel('Pattern')
+        ax1.set_xlabel("time (sec)")
+        ax1.set_ylabel("TTL state")
+        ax2.set_ylabel("Pattern")
         idx = 0
         while stime < etime:
-            plt.xlim(stime, stime+interval)
+            plt.xlim(stime, stime + interval)
             plt.savefig(os.path.join(save_path, f"ttl_state_{idx:04d}.png"))
             stime += interval
             idx += 1
-        plt.close('all')
+        plt.close("all")
 
     def plot_data_distribution(self, outputs, inputs, show=False, save_path=None):
         data, time = outputs
@@ -57,7 +57,7 @@ class BaseParseInput(OperatorMixin):
         plt.xlabel("Pattern Indes")
         plt.ylabel("Count")
         if save_path is not None:
-            plt.savefig(os.path.join(save_path, 'data_distribution.png'))
+            plt.savefig(os.path.join(save_path, "data_distribution.png"))
         if show:
             plt.show()
-        plt.close('all')
+        plt.close("all")
